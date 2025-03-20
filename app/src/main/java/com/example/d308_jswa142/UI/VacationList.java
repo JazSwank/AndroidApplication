c7df36292857c5c67d9a2bb23ea0892c3f240d39
Jazmine Swank <jswa142@wgu.edu>
2025-03-20 04:09:15 -0400

    C - working on pages and navigation

---
+++ app/src/main/java/com/example/d308_jswa142/UI/VacationList.java
@@ -0,0 +1,40 @@
+package com.example.d308_jswa142.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.d308_jswa142.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class VacationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_vacation_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(VacationList.this, VacationDetails.class);
+                    startActivity(intent);
+                }
+            });
+
+            System.out.println(getIntent().getStringExtra("test"));
+            return insets;
+
+        });
+    }
+}
