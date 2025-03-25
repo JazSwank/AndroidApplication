ab82b7dbeb50e6874594d023fe2a65ef47851b63
Jazmine Swank <jswa142@wgu.edu>
2025-03-25 02:21:33 -0400

    C - working on pages and navigation, vacation details and forms

--- app/src/main/java/com/example/d308_jswa142/UI/ExcursionDetails.java
+++ app/src/main/java/com/example/d308_jswa142/UI/ExcursionDetails.java
@@ -1,8 +1,6 @@
 package com.example.d308_jswa142.UI;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +9,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.d308_jswa142.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class VacationDetails extends AppCompatActivity {
+public class ExcursionDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_vacation_details);
+        setContentView(R.layout.activity_excursion_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(VacationDetails.this, ExcursionDetails.class);
-                    startActivity(intent);
-                }
-            });
             return insets;
         });
     }
